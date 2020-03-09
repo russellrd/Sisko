9f5c36ab44bd9b50ed14b6fa67107385503006e3
Russell Davidson <103starwars@gmail.com>
2020-03-08 20:25:05 -0400

    Added pygame base, owm and changed other files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Sisko
+
+## Installation
+
+Use the package manager pip to install requirments.
+
+```bash
+pip install -r requirements.txt
+```
--- app.py
+++ app.py
@@ -1,7 +1,41 @@
+import pyowm
+import pygame
+
+
 class Sisko:
+    def __init__(self):
+        pass
+
     def update(self):
-        print("working");
+        weather_surface = COMIC_SANS.render(
+            get_temp('Toronto, CA'), 0, (255, 255, 255))
+        screen.blit(weather_surface, (WIDTH/2-50, HEIGHT/2-20))
+
+
+def get_temp(loc):
+    location = owm.weather_at_place(loc)
+    return str(location.get_weather().get_temperature(
+        'celsius')['temp'])+"\u00b0"+"C"
+
 
 if __name__ == "__main__":
+    pygame.init()
+    pygame.display.set_caption('Sisko')
+
+    WIDTH = 800
+    HEIGHT = 600
+    COMIC_SANS = pygame.font.SysFont('Comic Sans MS', 40)
+
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    owm = pyowm.OWM('a1ffcf0881e1c11c07889b475f8cde59')
     sisko = Sisko()
-    sisko.update()
+    print("Sisko is now awake...")
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((100, 100, 0))
+        sisko.update()
+        pygame.display.update()
